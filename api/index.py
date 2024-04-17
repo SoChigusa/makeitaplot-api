80b8a4db9959f25bb19869e8e3080bbd75a5a1da
SoChigusa <so.chigusa.pp@gmail.com>
2024-04-16 20:38:49 -0700

    settings for 1D plot

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 import requests
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -15,8 +16,9 @@ def graph():
 
     # read file
     files = request.files
-    file = files.get('plot_data')
-    data_raw = file.read()
+    settings_raw = files.get('settings').read()
+    settings = json.loads(settings_raw.decode('utf-8'))
+    data_raw = files.get('plot_data').read()
 
     # data processing
     data = np.array([np.fromstring(line, sep='\t') for line in data_raw.splitlines()])
@@ -24,19 +26,32 @@ def graph():
 
     # plot
     matplotlib.use('agg')
-    fig = plt.figure(figsize=[8,6])
+    fig = plt.figure() if settings['fig']['size-no-specify'] else plt.figure(figsize=[settings['fig']['size'][0], settings['fig']['size'][1]])
     ax = fig.add_subplot(1,1,1)
-    ax.plot(data[:,0], data[:,1])
+    for plot in settings['plot']:
+        ax.plot(data[:,plot['x']],
+            data[:,plot['y']],
+            color=plot['color'],
+            ls=plot['line-style'],
+            lw=plot['line-width'])
     
-    # plot range
-    # ax.set_xlim(0, 1)
-    ax.set_ylim(0, 1)
+    # axis settings
+    if(not settings['x-axis']['lim-no-specify']):
+        ax.set_xlim(settings['x-axis']['lim'][0], settings['x-axis']['lim'][1])
+    if(not settings['y-axis']['lim-no-specify']):
+        ax.set_ylim(settings['y-axis']['lim'][0], settings['y-axis']['lim'][1])
+    if(settings['x-axis']['log-scale']):
+        ax.set_xscale('log')
+    if(settings['y-axis']['log-scale']):
+        ax.set_yscale('log')
 
     # title, labels
-    ax.set_title('Plot Title', size=20)
-    ax.set_xlabel('x-label', size=15)
-    ax.set_ylabel('y-label', size=15)
-    ax.tick_params(labelsize=15)
+    ax.set_title(settings['fig']['title'], size=settings['fig']['title-size'])
+    ax.set_xlabel(settings['x-axis']['label'],
+        size=settings['x-axis']['label-size'])
+    ax.set_ylabel(settings['y-axis']['label'],
+        size=settings['y-axis']['label-size'])
+    ax.tick_params(labelsize=settings['tick']['label-size'])
     
     # png data
     canvas = FigureCanvasAgg(fig)
